f530eaa7274b77f3394fad81669d1871216ca2a7
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-08-13 10:43:49 +0800

    added nav

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faSmile } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <nav>
+                <div className="container navigator d-flex align-items-center justify-content-between my-5">
+                    {/* Logo */}
+                    <div className="logo">
+                        <p>Foodify</p>
+                    </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                    {/* mobile - bars */}
+                    <div className="fs-1">
+                        <FontAwesomeIcon icon={faBars} />
+                    </div>
+                </div>
+            </nav>
+
+            <header>
+                <div className="container"></div>
+            </header>
+        </>
+    );
 }
 
-export default App
+export default App;
